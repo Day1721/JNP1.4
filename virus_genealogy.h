7e5025f30e06d02ca73f6497f5ada776a49f32f3
Maciej Godek <maciejgodek@gmail.com>
2016-12-15 19:58:54 +0100

    Node'y w vectorach dzieci i rodziców

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -17,35 +17,22 @@ class VirusGenealogy
 {
 private:
     typedef typename Virus::id_type ID;
-/*
-    struct ptr_comparator {
-        bool operator() (const std::shared_ptr<ID>& l, const std::shared_ptr<ID>& r) {
-            return *l < *r;
-        }
-    };
 
-    template <typename Enumerable, typename Function>
-    Enumerable select(Enumerable enumerable, Function handler) {
-        std::transform(enumerable.begin(), enumerable.end(), enumerable.begin(), handler);
-        return enumerable;
-    }*/
-
-    typedef std::vector<std::weak_ptr<ID>/*, ptr_comparator*/> IDSet;
+    struct Node;
+    typedef std::vector<std::weak_ptr<Node>> NodeVector;
 
     struct Node {
-        const std::shared_ptr<ID> id_ptr;
         Virus virus;
-        IDSet descendants;
-        IDSet ascendants;
+        NodeVector descendants;
+        NodeVector ascendants;
         Node(const ID& id)
-            : id_ptr(std::make_shared<ID>(id))
-            , virus(id)
+            : virus(id)
             , descendants()
             , ascendants()
         {}
     };
 
-    std::map<ID, std::unique_ptr<Node>> nodes;
+    std::map<ID, std::shared_ptr<Node>> nodes;
 
     ID _stem_id;
 
@@ -53,7 +40,7 @@ public:
     VirusGenealogy(const ID& stem_id)
     {
         _stem_id = stem_id;
-        nodes.insert(std::make_pair(stem_id, std::make_unique<Node>(stem_id)));
+        nodes.insert(std::make_pair(stem_id, std::make_shared<Node>(stem_id)));
     }
 
     ID get_stem_id() const
@@ -67,7 +54,7 @@ public:
             Node& current = *nodes.at(id);
             std::vector<ID> children;
             for (auto& x : current.descendants) {
-                children.push_back(*(x.lock()));
+                children.push_back(x->virus.get_id());
             }
             return children;
         }
@@ -83,7 +70,7 @@ public:
             Node& current = *nodes.at(id);
             std::vector<ID> parents;
             for (auto& x : current.ascendants) {
-                parents.push_back(*(x.lock()));
+                parents.push_back(x->virus.get_id());
             }
             return parents;
         }
@@ -104,17 +91,19 @@ public:
     }
 
     void create(const ID& id, const ID& parent_id)
-    {
+    { /*
         if(nodes.count(id) > 0) throw VirusAlreadyCreated();
         if(nodes.count(parent_id) == 0) throw VirusNotFound();
 
-        auto node = std::make_unique<Node>(id);
+        auto node = std::make_shared<Node>(id);
         node->ascendants.push_back(std::weak_ptr<ID>(nodes[parent_id]->id_ptr));
         nodes.insert(std::make_pair(id, std::move(node)));
+        */
     }
 
     void create(const ID& id, const std::vector<ID>& parent_ids)
     {
+        /*
         if(nodes.count(id) > 0) throw VirusAlreadyCreated();
         for(auto parent_id : parent_ids)
             if(nodes.count(parent_id) == 0) throw VirusNotFound();
@@ -123,29 +112,32 @@ public:
         for(auto& parent_id : parent_ids)
             node->ascendants.push_back(std::weak_ptr<ID>(nodes[parent_id]->id_ptr));
         nodes.insert(std::make_pair(id, std::move(node)));
+        */
     }
 
     void connect(const ID& child_id, const ID& parent_id)
     {
+        std::shared_ptr<Node> child, parent;
         try {
-            Node& child = *nodes.at(child_id);
-            Node& parent = *nodes.at(parent_id);
-            auto it = child.ascendants.begin();
-            while (it != child.ascendants.end() && it->lock() != parent.id_ptr) it++;
-            bool found = it != child.ascendants.end();
-            if(found) {
-                child.ascendants.push_back(std::weak_ptr<ID>(parent.id_ptr));
-                try {
-                    parent.descendants.push_back(std::weak_ptr<ID>(child.id_ptr));
-                }
-                catch (...) {
-                    child.ascendants.pop_back();
-                    throw;
-                }
-            }
+            child = nodes.at(child_id);
+            parent = nodes.at(parent_id);
         }
         catch (const std::out_of_range& oor) {
             //throw
+        }
+
+        auto it = child.ascendants.begin();
+        while (it != child.ascendants.end() && it->lock() != parent)
+            it++;
+        if(it != child.ascendants.end()) {
+            child.ascendants.push_back(std::weak_ptr<ID>(parent));
+            try {
+                parent.descendants.push_back(std::weak_ptr<ID>(child));
+            }
+            catch (...) {
+                child.ascendants.pop_back();
+                throw;
+            }
         }
     }
 
